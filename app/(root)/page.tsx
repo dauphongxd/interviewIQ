7e60afd7489ea8c98c748f6db6f9113b0d1a8451
Phong Dau <dauphongxd@gmail.com>
2025-04-24 11:03:31 -0500

    Added UI components for Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import {dummyInterviews} from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
     return (
-        <div>HomePage</div>
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Mock Interview with AI-Powered Practice & Feedback</h2>
+                    <p className="text-lg">
+                        Practice, refine, and outperform the competition.
+                    </p>
+
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interview</h2>
+
+                <div className="interview-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
+
+                <div className="interview-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+
+                    {/*<p>You haven&#39;t taken any interviews yet</p>*/}
+                </div>
+            </section>
+        </>
     )
 }
 export default Page
